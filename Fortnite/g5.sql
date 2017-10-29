c6967e34f12104306722d8d09f4ff98b8785521b
Steven Masley <stevenmasley@gmail.com>
2017-10-29 00:31:50 -0400

    Added mroe

--- Fortnite/g5.sql
+++ Fortnite/g5.sql
@@ -1,3 +1,13 @@
 -- Derby West
 INSERT INTO locations (name, type, geog, nonce)
                           VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(91.09375 41.21875)'),117338770);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(91.046875 40.78125)'),140173384);
+                          
+-- Derby House
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(87.328125 42.1875)'),173513267);
+
+
+
+
