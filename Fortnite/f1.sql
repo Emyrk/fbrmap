0f0b8fcedcc22cf253a73c4f0ecf59d77f8cca1c
Steven Masley <steven@slackerbots.com>
2017-10-28 23:20:38 -0400

    Adde 2 sqaures

---
+++ Fortnite/f1.sql
@@ -0,0 +1,2 @@
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(76.8125 86.84375)'),179239320);
---
+++ Fortnite/g2.sql
@@ -0,0 +1,3 @@
+-- BAMBOO HOUSE
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(87.9375 79.1875)'),143279214);
