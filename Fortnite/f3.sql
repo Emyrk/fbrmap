92aeec41563579969a1e6135bd7726f38a7f49db
Steven Masley <stevenmasley@gmail.com>
2017-10-29 00:08:55 -0400

    Added some more

---
+++ Fortnite/f3.sql
@@ -0,0 +1,5 @@
+INSERT INTO locations (name, type, geog, nonce)
+    VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(80.4375 61.90625)'),56479911);
+
+INSERT INTO locations (name, type, geog, nonce)
+    VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(72.421875 59.65625)'),114638190);     
