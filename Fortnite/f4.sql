014f35562edbcb2f203d5b65837055aa6b4fc4a9
Steven Masley <stevenmasley@gmail.com>
2017-10-29 00:41:19 -0400

    Added mroe

--- Fortnite/f4.sql
+++ Fortnite/f4.sql
@@ -1,6 +1,8 @@
 --- Camper Red barn
 INSERT INTO locations (name, type, geog, nonce)
                           VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(83.640625 49.8125)'),169829780);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(83.640625 49.62890625)'),159670320);               
 -- camper north
 INSERT INTO locations (name, type, geog, nonce)
                           VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(83.515625 51.71875)'),25717666);
@@ -12,3 +14,18 @@ INSERT INTO locations (name, type, geog, nonce)
 INSERT INTO locations (name, type, geog, nonce)
                           VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(77.5 45.15625)'),102512885);
 
+
+-- Retail north truck
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(76.37109375 48.98828125)'),56417499);
+-- Retail
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(75.6171875 48.85546875)'),89029159);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(77.49609375 47.828125)'),96310854);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(73.734375 45.4765625)'),44052524);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(74.2265625 45.4140625)'),128012224);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(71.84375 46)'),43696695);
