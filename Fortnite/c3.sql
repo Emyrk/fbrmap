115b797831f53b2c9df34cc7b52b586529e32803
Steven Masley <stevenmasley@gmail.com>
2017-10-29 10:56:35 -0400

    Added 2 chests

---
+++ Fortnite/c3.sql
@@ -0,0 +1,3 @@
+-- Loot Lake, Lake
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(41.21875 59)'),70079251);
--- Fortnite/d3.sql
+++ Fortnite/d3.sql
@@ -1,1 +1,7 @@
+-- Loot Lake, Lake
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(46.40625 62.453125)'),98321798);
 
+
+
+                          
