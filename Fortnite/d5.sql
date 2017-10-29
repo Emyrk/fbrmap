972d59117d4e19b64f112a5658ea69f34402e671
Steven Masley <stevenmasley@gmail.com>
2017-10-29 15:23:24 -0400

    Added salty

--- Fortnite/d5.sql
+++ Fortnite/d5.sql
@@ -2,17 +2,25 @@ INSERT INTO locations (name, type, geog, nonce)
                           VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(49.875 29.59375)'),36901706);
 
 
-
-
-
-
-
-
-
-
-
-
-
+-- Salty
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(54.984375 39.296875)'),205662042);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(57.0078125 38.4140625)'),183951020);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(57.109375 38.2265625)'),159132025);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(57.0234375 37.9921875)'),147600962);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(58.8984375 38.6328125)'),65806236);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(59.046875 38.9609375)'),148045030);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(56.1328125 40.65625)'),50671810);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(55.6875 40.671875)'),140914273);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(60.9140625 38.78125)'),101394199);
 
 
 
