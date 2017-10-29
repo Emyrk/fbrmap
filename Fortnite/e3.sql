72291e1a56b461c04325887949ad256f8698f06b
Steven Masley <stevenmasley@gmail.com>
2017-10-29 01:53:00 -0400

    Remove kingdom

--- Fortnite/e3.sql
+++ Fortnite/e3.sql
@@ -1,11 +1,12 @@
 INSERT INTO locations (name, type, geog, nonce)
                           VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(63.84375 63.828125)'),151790932);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(69.33984375 67.921875)'),75761774);
 
 -- Tomato Town Bridge
 INSERT INTO locations (name, type, geog, nonce)
                           VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(61.953125 67.296875)'),121412109);
 INSERT INTO locations (name, type, geog, nonce)
                           VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(65.390625 67.84375)'),61839973);
-
 
 
--- app/components/info-panel/info-panel.js
+++ app/components/info-panel/info-panel.js
@@ -32,6 +32,8 @@ export class InfoPanel extends Component {
 
   /** Create kingdom detail HTML string */
   async getKingdomDetailHtml (id) {
+    // Disable kingdoms
+    return
     // Get kingdom metadata
     let { kingdomSize, chestCount, kingdomSummary } = await this.api.getAllKingdomDetails(id)
 
