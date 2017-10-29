324d7a8e7fe2af7314f88597a55dbb69d50d5cf6
Steven Masley <stevenmasley@gmail.com>
2017-10-29 01:23:58 -0400

    Added battle bus and tree

---
+++ Fortnite/c5.sql
@@ -0,0 +1,7 @@
+-- Lucky Tree
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(36.59375 30.8359375)'),80525707);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(35.921875 30.859375)'),121438651);
+
+
