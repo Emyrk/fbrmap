4b7e90797710c520f486c29895dcf5b3dba60dd5
Steven Masley <stevenmasley@gmail.com>
2017-10-28 23:21:41 -0400

    Added b1

--- Fortnite/e6.sql
+++ Fortnite/e6.sql
@@ -15,3 +15,21 @@ INSERT INTO locations (name, type, geog, nonce)
 -- Truck stop small stack
 INSERT INTO locations (name, type, geog, nonce)
                           VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(65.265625 14.609375)'),206306052);
+
+
+-- Fatal Fields
+--	Longbarn
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(59.4375 27.1875)'),46153845);
+--	Shack by L
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(58.90625 24.125)'),58686582);
+--	Big House
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(58.796875 22.5625)'),68016287);
+--	Red Barn
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(60.828125 23.203125)'),51147114);
+
+
+
