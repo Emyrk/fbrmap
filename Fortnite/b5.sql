db925c986ebf421dac69e0c1ef24f148b7671169
Steven Masley <stevenmasley@gmail.com>
2017-10-29 02:12:23 -0400

    Added favicon

---
+++ Fortnite/b5.sql
@@ -0,0 +1,5 @@
+-- Greasy
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(23.671875 35.234375)'),208513791);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(24.625 35.390625)'),55498383);
