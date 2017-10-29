70a84413b1dc2a4187a3a10f9baf3aa7529a2846
Steven Masley <stevenmasley@gmail.com>
2017-10-29 14:09:28 -0400

    Added some more

--- Fortnite/f2.sql
+++ Fortnite/f2.sql
@@ -4,6 +4,16 @@ INSERT INTO locations (name, type, geog, nonce)
 INSERT INTO locations (name, type, geog, nonce)
                           VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(74.1640625 75.1796875)'),105852226);
 
-
+-- Top Right
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(79.6640625 81.6171875)'),2203763);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(79.5390625 81.390625)'),130223150);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(78.375 81.5234375)'),32853975);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(82.09375 82.71875)'),158607772);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(81.8359375 83.3828125)'),107677184);
 
 
