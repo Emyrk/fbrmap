45ee65baedb69ce695ebab039c0454a123433ad7
Steven Masley <stevenmasley@gmail.com>
2017-10-29 12:20:36 -0400

    Added loot lake

--- Fortnite/c3.sql
+++ Fortnite/c3.sql
@@ -1,3 +1,31 @@
 -- Loot Lake, Lake
 INSERT INTO locations (name, type, geog, nonce)
                           VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(41.21875 59)'),70079251);
+-- Loot Lake
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(38.609375 61.6875)'),150966315);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(38.5625 63.046875)'),11127510);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(34.5625 60.890625)'),201256151);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(37.734375 56.9375)'),41322825);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Fortnite/d3.sql
+++ Fortnite/d3.sql
@@ -3,5 +3,16 @@ INSERT INTO locations (name, type, geog, nonce)
                           VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(46.40625 62.453125)'),98321798);
 
 
+-- Loot Lake
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(41.671875 64.171875)'),115416836);                          
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(43.890625 62.671875)'),181050096);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(45.296875 68.546875)'),191609478);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(45.359375 67.109375)'),31971787);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(46.640625 65.234375)'),158896906);
 
                           
