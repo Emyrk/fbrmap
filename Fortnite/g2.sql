927c40cffb05ed91825b3b5c97d5c8cc44c2692d
Steven Masley <stevenmasley@gmail.com>
2017-10-29 14:08:17 -0400

    Added some more

--- Fortnite/g2.sql
+++ Fortnite/g2.sql
@@ -1,3 +1,13 @@
 -- BAMBOO HOUSE
 INSERT INTO locations (name, type, geog, nonce)
                           VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(87.9375 79.1875)'),143279214);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(88.32421875 79.14453125)'),25907626);
+INSERT INTO locations (name, type, geog, nonce)
+                          VALUES ('chest', 'chest',ST_GeographyFromText('SRID=4326;POINT(87.9140625 79.453125)'),73407109);
+
+
+
+
+
+                          
